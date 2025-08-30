8c98606f7aabe4d0715c72afe2d7d609971a1353
sal <salvador.cardenas.cruz@gmail.com>
2025-08-30 08:41:22 +0800

    Changed video thumb

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,7 +37,7 @@ export default function Hero() {
         playsInline
         aria-hidden="true"
         className="absolute inset-0 w-full h-full object-cover z-0 filter blur-[2px]"
-        poster="/banner-background-thumbnail.png"
+        poster="/banner-background-thumbnail-v2.png"
       >
         <source src="/banner-background.mp4" type="video/mp4" />
       </video>
