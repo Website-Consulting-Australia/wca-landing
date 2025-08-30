a46467120fadff6b34742b170963f80bd4a64bfa
sal <salvador.cardenas.cruz@gmail.com>
2025-08-31 07:07:16 +0800

    Updated icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/wca-ico.png" /*type="image/svg+xml"*/ />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
